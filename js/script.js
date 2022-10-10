ce58c712669407ab87888b7a1a4299dc52a8eca7
Omar-Codess <omartaki8@gmail.com>
2022-10-10 16:48:15 +0200

    pseudo codice

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Stampa numeri da 1 a 100
+
+// SE il numero stampato è multiplo di 3
+    // scrivi Fizz
+
+// ALTRIMENTI SE il numero stampato è multiplo di 5 
+    // scrivi Buzz
+
+// ALTRIMENTI SE il numero stampato è multiplo di 3 E di 5
+    // scrivi FizzBuzz
+
+// ALTRIMENTI stampa solo il numero
