7fab07df4109524104526cf1d5be652327ecb397
Omar-Codess <omartaki8@gmail.com>
2022-10-10 17:45:49 +0200

    Aggiunta condizione per FizzBuzz

--- js/script.js
+++ js/script.js
@@ -2,28 +2,26 @@
 for (let i = 1; i <= 100; i++){
     console.log(i);
 
-// SE il numero stampato è multiplo di 3
-    // scrivi Fizz
     let multiplo = i;
-    if (i % 3 === 0) {
+
+    // SE il numero stampato è multiplo di 3 E di 5
+        // scrivi FizzBuzz
+    if (i % 3 === 0 && i % 5 === 0) {
+        multiplo = "FizzBuzz";
+        console.log(multiplo);
+    }
+
+    // SE il numero stampato è multiplo di 3
+        // scrivi Fizz
+    else if (i % 3 === 0) {
         multiplo = "Fizz";
         console.log(multiplo);
     }   
 
-// ALTRIMENTI SE il numero stampato è multiplo di 5 
-    // scrivi Buzz
+    // SE il numero stampato è multiplo di 5
+        // scrivi Buzz
     else if (i % 5 === 0) {
         multiplo = "Buzz";
         console.log(multiplo);
     }
 
-// ALTRIMENTI SE il numero stampato è multiplo di 3 E di 5
-    // scrivi FizzBuzz
-    else if (i % 3 === 0 || i % 5 === 0) {
-        multiplo = "FizzBuzz";
-        console.log(multiplo);
-    }
-
-// ALTRIMENTI stampa solo il numero
-
-}
